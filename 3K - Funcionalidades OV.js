37a46b414eb1aa16aedd01e19074644ac167604b
Guru <sebastian.ulacco@3ksys.com.ar>
2019-04-25 12:22:04 -0300

    Modificacion para realizar la inactivacion de las Requisiciones de Compras

--- 3K - Funcionalidades OV.js
+++ 3K - Funcionalidades OV.js
@@ -11415,6 +11415,37 @@ define(['N/error', 'N/search', 'N/record', 'N/format', 'N/task', 'N/http', 'N/ru
 
                     }
 
+                    // INICIO - Inactivar Requisiciones Asociadas
+                    var numLinesRequisiciones = rec.getLineCount({
+                        sublistId: 'recmachcustrecord_3k_req_compra_ov'
+                    });
+                    if(!utilities.isEmpty(numLinesRequisiciones) && numLinesRequisiciones > 0 ){
+                        for(var j = 0; j < numLinesRequisiciones; j++){
+                            var lineNum = rec.selectLine({
+                                sublistId: 'recmachcustrecord_3k_req_compra_ov',
+                                line: j
+                            });
+                            var ordenCompraAsociada = rec.getCurrentSublistValue({
+                                sublistId: 'recmachcustrecord_3k_req_compra_ov',
+                                fieldId: 'custrecord_3k_req_compra_oc'
+                            });
+
+                            if (utilities.isEmpty(ordenCompraAsociada)) {
+                                rec.setCurrentSublistValue({
+                                    sublistId: 'recmachcustrecord_3k_req_compra_ov',
+                                    fieldId: 'isinactive',
+                                    value: true,
+                                    ignoreFieldChange: true
+                                });
+
+                                rec.commitLine({
+                                    sublistId: 'recmachcustrecord_3k_req_compra_ov'
+                                });
+                            }
+                        }
+                    }
+                    // FIN - Inactivar Requisiciones Asociadas
+
                     rec.save();
                     return objRespuesta;
 
